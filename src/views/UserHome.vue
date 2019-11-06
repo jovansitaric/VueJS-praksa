e6fcba8d8a14f29a9eba2ca53110b2bf113e4711
Praksa 1 <praksa.1@itengine.eu>
2019-11-06 10:55:17 +0100

    Implemented bearer token request

--- src/views/UserHome.vue
+++ src/views/UserHome.vue
@@ -2,7 +2,7 @@
   <div class="card">
     <div class="card-image">
       <figure class="image is-4by3">
-        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
       </figure>
     </div>
     <div class="card-content">
@@ -16,55 +16,74 @@
       <div class="content">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Phasellus nec iaculis mauris.
-        <br>
+        <br />
       </div>
+      <div id="test">
+          {{title}}
+          {{description}}
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name: 'UserHome',
-    mounted () {
-      if (localStorage.getItem('access_token')) {
-        console.log("Token in use");
-        this.getUserData();
-      } 
-      else {
-        console.log("sign in");
-        this.$router.push({ name: '/' })
-      }
-    },    
-    data () {
-      return {
-        tasks: []
-      }
-    },
-    methods: {
-      getUserData() {
-        const t = this        
-        axios.get("http://praksa.test/api/tasks")
-             .then(function (res) {
-               t.tasks = res.data.id
-              //  console.log(this.getTemp)
-               console.log(res)
-               console.log(t.tasks)
-             })
-             .catch((e) => {
-                console.error(e)
-              })
-      }
+  name: "UserHome",
+  mounted() {
+    this.fetchUserDetails();
+    // if (localStorage.getItem('access_token')) {
+    //   console.log("Token in use");
+    //   this.getUserData();
+    // }
+    // else {
+    //   console.log("sign in");
+    //   this.$router.push({ name: '/' })
+    // }
+  },
+  data() {
+    return {
+      title: "",
+      description: ""
+    };
+  },
+  methods: {
+    fetchUserDetails() {
+      const access_token = localStorage.getItem("access_token");
+      const URL = `http://praksa.test/api/tasks`;
+      axios({
+        method: "get",
+        url: URL,
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + access_token
+        }
+      }).then(res => {
+        this.title = res.data.data.title
+        this.description = res.data.data.description
+        console.log(res)
+        console.log(this.title)
+        console.log(access_token)
+      });
+      // axios.get("http://praksa.test/api/tasks")
+      //      .then(function (res) {
+      //        t.tasks = res.data.id
+      //       //  console.log(this.getTemp)
+      //        console.log(res)
+      //        console.log(t.tasks)
+      //      })
+      //      .catch((e) => {
+      //         console.error(e)
+      //      })
     }
-}
-
+  }
+};
 </script>
 
 <style>
-  .card {
-    width: 20%;
-    margin: 0 auto;
-  }
+.card {
+  width: 20%;
+  margin: 0 auto;
+}
 </style>
